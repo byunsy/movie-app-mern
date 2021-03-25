b497d8b017f281aaf74960a011149fcde85abf77
unknown <bsyiskl@gmail.com>
2021-03-25 20:22:13 +0900

    initial commit

--- server/config/key.js
+++
@@ -1,6 +0,0 @@
-// Checks whether the current node environment is 'production' or 'development'
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./prod')
-} else {
-    module.exports = require('./dev')
-}
--- server/config/prod.js
+++
@@ -1,4 +0,0 @@
-// Enables access to MongoDB using ID and PW during production mode.
-module.exports = {
-    mongoURI: process.env.MONGO_URI
-}

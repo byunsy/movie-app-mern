f79de4318086a086014af510d596120357b2003e
unknown <bsyiskl@gmail.com>
2021-03-26 23:09:34 +0900

    added movie database APIs

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,5 +1,8 @@
 // Server Routes
 export const USER_SERVER = "/api/users";
-
 export const API_URL = "https://api.themoviedb.org/3/";
 export const IMG_BASE_URL = "https://image.tmdb.org/t/p/";
+
+// Acquire movie API key from key.js
+const key = require("./key");
+export const API_KEY = key.MOVIE_API;

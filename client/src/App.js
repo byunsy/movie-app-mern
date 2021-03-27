a65ece81e20b7696b94cb367025cc537932415dd
unknown <bsyiskl@gmail.com>
2021-03-27 20:45:02 +0900

    added grid-card display of movies in landing page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LandingPage from "./components/views/LandingPage/LandingPage";
 import LoginPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import MovieDetail from "./components/views/MovieDetail/MovieDetail";
 import NavBar from "./components/views/NavBar/NavBar";
 import Auth from "./hoc/auth";
 import { Layout } from "antd";
@@ -19,6 +20,7 @@ function App() {
             <Route exact path="/" component={Auth(LandingPage, null)} />
             <Route exact path="/login" component={Auth(LoginPage, false)} />
             <Route exact path="/register" component={Auth(RegisterPage, false)} />
+            <Route exact path="/movies/:movieID" component={Auth(MovieDetail, null)} />
           </Switch>
         </Router>
       </Content>
--- client/src/components/views/Commons/GridCards.js
+++ client/src/components/views/Commons/GridCards.js
@@ -5,7 +5,7 @@ function GridCards(props) {
   return (
     <Col lg={6} md={8} xs={24}>
       <div style={{ position: "relative" }}>
-        <a href={`/movie/${props.movieID}`}>
+        <a href={`/movies/${props.movieID}`}>
           <img style={{ width: "100%" }} src={props.image} alt={props.movieName} />
         </a>
       </div>

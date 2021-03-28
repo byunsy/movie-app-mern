23e8dffafb45629d7bc66a838bfd2af041c0f77b
unknown <bsyiskl@gmail.com>
2021-03-28 17:24:21 +0900

    added grid-card display of movie cast

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
 import { API_URL, API_KEY, IMG_BASE_URL } from "../../../Config";
-import { Button } from "antd";
+import { Button, Row } from "antd";
 import MainImage from "../LandingPage/Sections/MainImage";
 import MovieInfo from "./Sections/MovieInfo";
+import GridCards from "../Commons/GridCards";
 
 function MovieDetail(props) {
   let movieID = props.match.params.movieID;
   const [Movie, setMovie] = useState([]);
+  const [Cast, setCast] = useState([]);
 
   useEffect(() => {
-    let endpointCrew = `${API_URL}movie/${movieID}/credits?api_key=${API_KEY}`;
+    // TMDB movie APIs
+    let endpointCast = `${API_URL}movie/${movieID}/credits?api_key=${API_KEY}`;
     let endpointInfo = `${API_URL}movie/${movieID}?api_key=${API_KEY}`;
 
+    // Fetch movie information from API
     fetch(endpointInfo)
       .then((response) => response.json())
       .then((response) => {
         setMovie(response);
+      });
+
+    // Fetch cast information from API
+    fetch(endpointCast)
+      .then((response) => response.json())
+      .then((response) => {
+        setCast(response.cast);
       });
   });
 
@@ -35,8 +46,19 @@ function MovieDetail(props) {
         <br />
         {/* Actors Grid */}
         <div style={{ display: "flex", justifyContent: "center", margin: "2rem" }}>
-          <Button>Toggle Actor View</Button>
+          <Button>View Actors</Button>
         </div>
+        <Row gutter={[24, 24]}>
+          {Cast &&
+            Cast.map((cast, index) => (
+              <React.Fragment key={index}>
+                <GridCards
+                  image={cast.profile_path ? `${IMG_BASE_URL}w500${cast.profile_path}` : null}
+                  characterName={cast.name}
+                />
+              </React.Fragment>
+            ))}
+        </Row>
       </div>
     </div>
   );

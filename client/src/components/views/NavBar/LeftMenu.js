36bef003b2eb355fb64d5b2009c836569bdf1976
unknown <bsyiskl@gmail.com>
2021-03-31 22:25:52 +0900

    added sign-out button when signed in

--- client/src/components/views/NavBar/LeftMenu.js
+++ client/src/components/views/NavBar/LeftMenu.js
@@ -10,7 +10,7 @@ function LeftMenu() {
       <Menu.Item key="home">
         <a href="/">Home</a>
       </Menu.Item>
-      <SubMenu title={<span>Projects</span>}>
+      {/* <SubMenu title={<span>Projects</span>}>
         <MenuItemGroup title="Item 1">
           <Menu.Item key="setting:1">Option 1</Menu.Item>
           <Menu.Item key="setting:2">Option 2</Menu.Item>
@@ -19,7 +19,7 @@ function LeftMenu() {
           <Menu.Item key="setting:3">Option 3</Menu.Item>
           <Menu.Item key="setting:4">Option 4</Menu.Item>
         </MenuItemGroup>
-      </SubMenu>
+      </SubMenu> */}
       <Menu.Item key="favorite">
         <a href="/favorites">My Favorite</a>
       </Menu.Item>
